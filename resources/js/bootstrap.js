df2d761e590f9865edd9082ec8ebcd505f96f9a4
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-03-28 08:03:48 +0000

    jsファイル修正

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -17,13 +17,13 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import Echo from 'laravel-echo';
 
-window.Pusher = require('pusher-js');
+// window.Pusher = require('pusher-js');
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    client:process.env.MIX_PUSHER_APP_ID,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: true,
-    encrypted: true
-});
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     client:process.env.MIX_PUSHER_APP_ID,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     forceTLS: true,
+//     encrypted: true
+// });
