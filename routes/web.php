c1010e14e1aba72bd913a2433b4f077edeee045a
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-04-04 11:57:33 +0000

    routes修正

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', 'ArticleController@index')->name('articles.index');
     //いいねした記事を表示
