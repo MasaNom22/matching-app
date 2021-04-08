7e2a5b489e05f6127dc52bccff309b0efc276aef
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-04-08 13:45:59 +0000

    トップページにログインユーザーの情報を表示

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,6 +8,20 @@
   <div class="row d-flex justify-content-center">
     <div class="row col-md-12">
       <aside class="col-3 d-none d-md-block position-fixed">
+        <div style="" class="">
+          @if(isset(Auth::user()->uploadimages))
+          <a href="{{ route('users.show', ['id' => Auth::user()->id]) }}" class="text-dark">
+	        <img class="user-icon rounded-circle" src="{{ Storage::url(Auth::user()->uploadimages->file_path) }}" style="width:100%;"　alt="写真"/>
+	        </a>
+            @else
+            <a href="{{ route('users.show', ['id' => Auth::user()->id]) }}" class="text-dark">
+            <i class="fas fa-user-circle fa-9x mr-1"></i>
+            </a>
+            @endif
+            <h4>{{ Auth::user()->name }}  さん</h4>
+	        <h5>年齢: {{ Auth::user()->age }}</h5>
+	        <h5>性別: {{ Auth::user()->gender_label }}</h5>
+        </div>
       </aside>
       <main class="col-md-7 offset-md-5">
           @foreach($articles as $article)
