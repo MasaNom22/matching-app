55f92f61eca8c8eba997033d5d17f3c03dce885e
masakatsu.nomura <masakatsunomura26@gmail.com>
2021-04-22 12:16:26 +0000

    記事編集画面レスポンシブ

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -5,7 +5,7 @@
 @section('content')
   <div class="container my-5">
     <div class="row">
-      <div class="mx-auto col-7">
+      <div class="mx-auto col-md-7">
         <div class="card">
           <h2 class="h4 card-header text-center aqua-gradient text-white">コメント編集</h2>
           <div class="card-body pt-3">
